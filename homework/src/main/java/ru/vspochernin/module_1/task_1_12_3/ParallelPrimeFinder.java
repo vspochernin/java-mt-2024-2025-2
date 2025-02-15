3bfbd158c4b06c9262eb1c8247b3d743fccead53
Vladislav Pochernin <vspochernin@gmail.com>
2025-02-15 17:35:06 +0300

    hw1: optimize prime fining algorithm

--- homework/src/main/java/ru/vspochernin/module_1/task_1_12_3/ParallelPrimeFinder.java
+++ homework/src/main/java/ru/vspochernin/module_1/task_1_12_3/ParallelPrimeFinder.java
@@ -67,7 +67,13 @@ class ParallelPrimeFinder {
         if (n == 1) {
             return false;
         }
-        for (long i = 2; i < n; i++) {
+        if (n <= 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+        for (long i = 5; i*i <= n; i++) {
             if (n % i == 0) {
                 return false;
             }
