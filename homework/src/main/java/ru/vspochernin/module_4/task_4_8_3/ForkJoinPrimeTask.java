4e9dfc23c08d09f3f79c6e1ed244655fdc45fa59
Vladislav Pochernin <vspochernin@gmail.com>
2025-03-07 16:55:12 +0300

    hw4: initial commit for task3

---
+++ homework/src/main/java/ru/vspochernin/module_4/task_4_8_3/ForkJoinPrimeTask.java
@@ -0,0 +1,11 @@
+package ru.vspochernin.module_4.task_4_8_3;
+
+import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.ForkJoinPool;
+
+class ForkJoinPrimeTask {
+    public static long countPrimes(long l, long r) {
+        // Ваше решение
+        return 42L;
+    }
+}
